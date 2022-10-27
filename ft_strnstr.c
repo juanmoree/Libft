cee1e4c25558198b48ef40850bcf8c8d2b6369fc
Juan Camilo Moreno Ceballos <jumoreno@car10s3.42barcelona.com>
2022-10-27 21:35:15 +0200

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jumoreno <jumoreno@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:52:22 by jumoreno          #+#    #+#             */
-/*   Updated: 2022/10/26 22:52:33 by jumoreno         ###   ########.fr       */
+/*   Updated: 2022/10/27 21:34:51 by jumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,33 @@
 #include <stdio.h>
 char    *ft_strnstr(const char *h, const char *n, size_t len)
 {
-    size_t	i;
-    size_t	j;
+    size_t    i;
+    size_t    j;
 
     i = 0;
     j = 0;
-	if (((const char *)n)[i] == '\0')
-		return ((char *)h);
-	while (n[i] && h[j] && j <= len)
+    if (((const char *)n)[i] == '\0')
+      return ((char *)h);
+    while (((const char *)n)[i] != '\0' && j < (len - 1))
     {
-        if (n[i] != h[j])
+          if (n[i] == h[j])
+          {
+            i++;
             j++;
-        while (n[i] == h[j])
-        {
-          if (n[i + 1] == h [j + 1])
-          return ((char *)&h[j]);
+          }
+          if (((const char *)n)[i] == '\0')
+          return ((char *)&h[j - 1]);
           else
           j++;
-        }
     }
-    return ((char *)h);
+    return (NULL);
 }
 /*
 int    main()
 {
-    char    s1[] = "Cadena a apuntar";
-    char    s2[] = "x";
-    size_t    len = 11;
+    char    s1[] = "see FF your FF return FF now FF";
+    char    s2[] = "FF";
+    size_t    len = 20;
     printf("%s\n", ft_strnstr(s1, s2, len));
 	printf("%s\n", strnstr(s1, s2, len));
 }*/
