5bc5287a9aa7614c81fcdc2c5629ce668ed76c49
Juan Camilo Moreno Ceballos <jumoreno@car10s3.42barcelona.com>
2022-10-31 20:19:26 +0100

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,41 +6,32 @@
 /*   By: jumoreno <jumoreno@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 18:52:22 by jumoreno          #+#    #+#             */
-/*   Updated: 2022/10/27 21:34:51 by jumoreno         ###   ########.fr       */
+/*   Updated: 2022/10/31 20:19:04 by jumoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-char    *ft_strnstr(const char *h, const char *n, size_t len)
-{
-    size_t    i;
-    size_t    j;
 
-    i = 0;
-    j = 0;
-    if (((const char *)n)[i] == '\0')
-      return ((char *)h);
-    while (((const char *)n)[i] != '\0' && j < (len - 1))
-    {
-          if (n[i] == h[j])
-          {
-            i++;
-            j++;
-          }
-          if (((const char *)n)[i] == '\0')
-          return ((char *)&h[j - 1]);
-          else
-          j++;
-    }
-    return (NULL);
-}
-/*
-int    main()
+char	*ft_strnstr(const char *h, const char *n, size_t len)
 {
-    char    s1[] = "see FF your FF return FF now FF";
-    char    s2[] = "FF";
-    size_t    len = 20;
-    printf("%s\n", ft_strnstr(s1, s2, len));
-	printf("%s\n", strnstr(s1, s2, len));
-}*/
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (n[0] == '\0')
+		return ((char *)h);
+	while (h[i] != '\0')
+	{
+		j = 0;
+		while (h[i + j] == n[j] && (i + j) < len)
+		{
+			if (h[i + j] == '\0' && n[j] == '\0')
+				return ((char *)&h[i]);
+			j++;
+		}
+		if (n[j] == '\0')
+			return ((char *)h + i);
+		i++;
+	}
+	return (0);
+}
